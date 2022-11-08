df817c86f9556f17a9882b56a605ff9788ea656c
CorvusOps <josepholivar124@gmail.com>
2022-11-08 14:48:42 +0800

    Add Initial Includes
     Appended connection and disconnection to the database and an error page

---
+++ src/includes/logout.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+
+	// Unset all of the session variables
+	$_SESSION = array();
+	
+	session_destroy();
+
+	/*
+	 * Change this pathing when the login client is pushed
+	 */
+	header("location: ../../index.html");
+?>
