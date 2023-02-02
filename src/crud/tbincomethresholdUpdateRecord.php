f77356bde2c5606a14c0865459ba8e90f189f454
Murasakinosora <joshuatolentino44444@gmail.com>
2023-02-02 20:56:23 +0800

    incomeThreshold Crud + some adjustments

---
+++ src/crud/tbincomethresholdUpdateRecord.php
@@ -0,0 +1,26 @@
+<?php
+include '../includes/connectdb.php';
+
+if(isset($_GET["tbItID"]) && !empty($_GET["tbItID"])){
+    $tbItID = $_GET['tbItID'];
+    $clITYear = $_POST['clITYear'];
+    $tbItPercent = $_POST['tbItPercent'];
+    $updatequery = "UPDATE tbincomethreshold SET tbItPercent = '$tbItPercent', clITYear = '$clITYear' WHERE tbItID ='$tbItID';";
+   
+    
+    if(mysqli_query($connectdb, $updatequery)){
+        echo "<script> 
+        alert('Record successfully updated!'); 
+        window.location = '../city/cityITRecords.php'; 
+        </script>";  
+    
+    } else{
+        echo "<script>
+        alert('Failed to update.');  
+        window.location = '../city/cityITRecords.php';
+        </script>";  
+    }
+
+}
+
+?>
